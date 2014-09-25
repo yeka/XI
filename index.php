6934a2270f7d50c1058675df25f5f2348350d988
Yakub Kristianto <yakub@rumah.com>
2014-09-25 11:49:38 +0700

    add routing matcher

--- index.php
+++ index.php
@@ -31,6 +31,14 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
+use Symfony\Component\Routing\Loader\YamlFileLoader as YamlRoutingLoader;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing\Route;
+
+use Symfony\Component\HttpFoundation\Request;
+
 function example1()
 {
 	// Manual usage - symfony/dependency-injection
@@ -58,6 +66,32 @@ function example2()
 	echo $abc->hello();
 }
 
-example2();
-echo "\n".memory_get_peak_usage();
-echo "\n".(microtime(true) - $time);
+function exampleRouting1()
+{
+	// look inside *this* directory
+	$locator = new FileLocator(array(__DIR__));
+	$loader = new YamlRoutingLoader($locator);
+	$routes = $loader->load('routes.yml');
+	
+	$request = Request::createFromGlobals();
+	$context = new RequestContext();
+	$context->fromRequest($request);
+
+	$matcher = new UrlMatcher($routes, $context);
+
+	$parameters = $matcher->match('/foo');
+	print_r($parameters);
+
+	try {
+		$parameters = $matcher->matchRequest($request);
+		print_r($parameters);
+	} catch (\Exception $e) {
+		echo "No route match";
+	}
+}
+
+//example1();
+//example2();
+exampleRouting1();
+echo "\n".number_format(memory_get_peak_usage() / 1024 / 1024, 4).' MB';
+echo "\n".number_format(microtime(true) - $time, 4).' s';
